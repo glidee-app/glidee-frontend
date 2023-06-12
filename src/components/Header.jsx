b90708890bc4b328a762a98c953b928136a0968b
Quyum Kehinde <quyumkehinde@gmail.com>
2023-06-12 15:43:04 +0100

    feat: added mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from '../assets/images/logo.png';
 import auth from "../services/auth";
 
 const Header = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
-      <header className="bg-primary text-white flex justify-between items-center p-5 md:p-10">
-        <NavLink to="/" >
-          <img src={logo} className="w-12 inline-block mr-2" />
+      <header className="bg-primary text-white flex items-start lg:items-center justify-between p-5 md:p-10">
+        <NavLink to="/" className="flex items-center">
+          <img src={logo} className="lg:w-12 w-8 inline-block mr-2" />
           <span className="text-2xl font-medium">Glidee</span>
         </NavLink>
-        <nav>
+        <nav className="hidden lg:block">
           {auth.isTokenExpired()
             ?
             <>
@@ -41,6 +47,24 @@ const Header = () => {
             </>
           }
         </nav>
+        <div className="lg:hidden block text-lg">
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          {isMenuOpen &&
+            <nav className="flex flex-col bg-white text-secondary absolute top-16 w-full right-0">
+              <NavLink to="/" className={({ isActive }) =>
+                `${isActive && "text-gray-500"} p-3`
+              }>Home</NavLink>
+              <NavLink to="/login" className={({ isActive }) =>
+                `${isActive && "text-gray-500"} p-3`
+              }>Login</NavLink>
+              <NavLink to="/signup" className={({ isActive }) =>
+                `${isActive && "text-gray-500"} p-3 pr-0`
+              }>Signup</NavLink>
+            </nav>
+          }
+        </div>
       </header>
     </>
   )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   return (
     <>
       <Header />
-      <main className='flex justify-center py-28 bg-primary text-neutral-50 min-h-screen'>
+      <main className='flex justify-center py-28 px-5 bg-primary text-neutral-50 min-h-screen'>
         <div className="w-full max-w-4xl">
           <h2 className="lg:text-3xl text-2xl font-semibold">Dashboard</h2>
           <h3 className="text-lg mt-10">Hello, {user.sub.user_name}. How can we help you today?</h3>
