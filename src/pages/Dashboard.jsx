9facdff62b50e490fc55709954893289fca166ee
Quyum Kehinde <quyumkehinde@gmail.com>
2023-06-12 11:50:54 +0100

    fix: update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,11 +9,18 @@ const Dashboard = () => {
   return (
     <>
       <Header />
-      <main className='flex justify-center items-center py-28 flex-col bg-primary text-neutral-50'>
-        <h2>Hello, {user.sub.user_name}</h2>
-        <div className='flex justify-between items-center w-full md:w-2/5 font-medium my-28'>
-          <Link to="/book-ride" className='p-10 px-4 rounded-lg border border-white'>Book Ride</Link>
-          <Link to="/order-history" className='p-10 px-4 rounded-lg border border-secondary bg-secondary'>View Order History</Link>
+      <main className='flex justify-center py-28 bg-primary text-neutral-50 min-h-screen'>
+        <div className="w-full max-w-4xl">
+          <h2 className="lg:text-3xl text-2xl font-semibold">Dashboard</h2>
+          <h3 className="text-lg mt-10">Hello, {user.sub.user_name}. How can we help you today?</h3>
+          <ul className='font-medium my-10 list-disc list-inside'>
+            <li className="my-2">
+              <Link to="/book-ride" className="text-blue-500 hover:underline">Book Ride</Link>
+            </li>
+            <li className="my-2">
+              <Link to="/order-history" className="text-blue-500 hover:underline">View Order History</Link>
+            </li>
+          </ul>
         </div>
       </main>
     </>
